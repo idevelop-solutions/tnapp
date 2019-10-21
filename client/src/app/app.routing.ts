276c708765075df3dafc8b35f6aeb303cb57f1ed
zafarshafaq <zafarshafaq34@gmail.com>
2019-10-21 14:37:55 +0430

    #7 Setting of login page as default route

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -12,8 +12,10 @@ import { RegisterComponent } from './views/register/register.component';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
+    component: LoginComponent,
+    data: {
+      title: 'Login Page'
+    }
   },
   {
     path: '404',
@@ -30,18 +32,16 @@ export const routes: Routes = [
     }
   },
   {
-    path: 'login',
-    component: LoginComponent,
-    data: {
-      title: 'Login Page'
-    }
-  },
-  {
     path: 'register',
     component: RegisterComponent,
     data: {
       title: 'Register Page'
     }
+  },
+  {
+    path: 'dashboard',
+    redirectTo: '/dashboard', 
+    pathMatch: 'full',
   },
   {
     path: '',
