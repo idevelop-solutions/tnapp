dab7bbae0f09c1fa8acca0391fed88b0e3246e9f
zafarshafaq <zafarshafaq34@gmail.com>
2019-10-30 16:58:49 +0430

    #7  Mail Configuration

--- blog/app/Http/Controllers/PasswordResetController.php
+++ blog/app/Http/Controllers/PasswordResetController.php
@@ -3,10 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ResetPasswordMail;
 class PasswordResetController extends Controller
 {
-    sendEmail(){
-        
+    public function sendEmail(Request $request){
+        if(!$this->validateEmail($request->email)){
+
+            return $this->faildResponse();
+        }
+        $this->send($request->email);
+        return $this->successResponse();
+
     }
+
+    public function send($email){
+        Mail::to($email)->send(new ResetPasswordMail);
+
+    }
+
+    public function validateEmail($email){
+        return !!User::where('email',$email)->first();
+    }
+
+    public function faildResponse(){
+
+        return response()->json([
+            'error' => 'Email not exist in our records!'
+        ],Response::HTTP_NOT_FOUND);
+    }
+
+    public function successResponse(){
+        return response()->json([
+            'data' => 'Password Reset Link sent to your email'
+        ],Response::HTTP_OK);
+    }
+
+
 }
